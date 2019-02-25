3574c167e8a8c015b6daf0636a10b92eb3db1f1e
Jake Shams <jshams62@gmail.com>
2019-02-24 18:52:56 -0800

    added keyboard controlls

--- src/GameScene.js
+++ src/GameScene.js
@@ -18,6 +18,7 @@ class GameScene extends Scene {
 
     this.createPlatforms()
     this.createPlayer()
+    this.createCursor()
   }
 
   createPlatforms() {
@@ -55,6 +56,25 @@ class GameScene extends Scene {
   }
 
 
+  createCursor() {
+    this.cursors = this.input.keyboard.createCursorKeys()
+  }
+
+  update() {
+    if (this.cursors.left.isDown) {
+        this.player.setVelocityX(-160);
+        this.player.anims.play('left', true);
+    } else if (this.cursors.right.isDown) {
+        this.player.setVelocityX(160);
+        this.player.anims.play('right', true);
+    } else {
+      this.player.setVelocityX(0);
+        this.player.anims.play('turn');
+    }
+    if (this.cursors.up.isDown && this.player.body.touching.down) {
+        this.player.setVelocityY(-330);
+    }
+  }
 
 
 
