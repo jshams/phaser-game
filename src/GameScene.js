b8eb893bead0f005167af6d7d3eb286804a6bb61
Jake Shams <jshams62@gmail.com>
2019-02-24 18:20:13 -0800

    added assets

--- src/GameScene.js
+++ src/GameScene.js
@@ -2,7 +2,14 @@ import { Scene } from 'phaser'
 
 class GameScene extends Scene {
   preload() {
-    this.load.image('logo', 'assets/logo.png');
+    this.load.image('sky', 'assets/sky.png');
+    this.load.image('ground', 'assets/platform.png');
+    this.load.image('star', 'assets/star.png');
+    this.load.image('bomb', 'assets/bomb.png');
+    this.load.spritesheet('dude',
+        'assets/dude.png',
+        { frameWidth: 32, frameHeight: 48 }
+    );
   }
 
   create() {
