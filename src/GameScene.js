265f2e8cf4075c56e35656e3e83a20ed76461ba3
Jake Shams <jshams62@gmail.com>
2019-03-02 00:13:29 -0800

    return to preload after gameover

--- src/GameScene.js
+++ src/GameScene.js
@@ -133,6 +133,7 @@ class GameScene extends Scene {
       player.anims.play('turn');
       this.gameOver = true;
       this.gameOverText.visible = true
+      this.input.on('pointerdown', () => this.scene.start('preload'))
   }
 
   ///////////////////
